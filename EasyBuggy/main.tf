2c90058230e6fea870422d11b934e454368974af
vagrant <keig0xurita@gmail.com>
2024-08-06 08:40:08 +0000

    deploy completed!

--- EasyBuggy/main.tf
+++ EasyBuggy/main.tf
@@ -13,6 +13,7 @@ provider "google" {
   region      = var.region
 }
 
+
 resource "google_compute_instance" "easybuggy" {
   name         = "easybuggy-instance"
   machine_type = "n1-standard-1"
@@ -25,23 +26,55 @@ resource "google_compute_instance" "easybuggy" {
   }
 
   network_interface {
-    network = "default"
+    network = "default"    
     access_config {}
   }
+
+  metadata_startup_script = <<EOF
+#!/bin/bash
+sudo apt-get update
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+sudo apt-get install -y ca-certificates curl git 
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo git clone https://github.com/k-tamura/easybuggy.git
+cd easybuggy
+sudo docker build . -t easybuggy:local 
+sudo docker run -p 8080:8080 easybuggy:local 
+EOF
+}
+
+resource "google_compute_firewall" "allow-home-ip" {
+  name    = "allow-home-ip"
+  network = "default" 
+
+  allow {
+    protocol = "tcp"
+    ports    = ["8080"]
+  }
+
+  source_ranges = [var.my_ip]
+}
+
+resource "google_compute_firewall" "allow-ssh" {
+  name    = "allow-ssh"
+  network = "default"  
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["35.235.240.0/20"]
 }
 
 output "instance_ip" {
   value = google_compute_instance.easybuggy.network_interface[0].access_config[0].nat_ip
-}
-
-resource "google_compute_firewall" "default" {
-  name    = "allow-home-ip"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "443", "8080"]
-  }
-
-  source_ranges = [var.my_ip]
 }
