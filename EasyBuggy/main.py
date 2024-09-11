0b5d3291f250f5fae99bd8cb7df10842ce3dcf75
vagrant <keig0xurita@gmail.com>
2024-09-11 09:53:38 +0000

    update

--- EasyBuggy/main.py
+++ EasyBuggy/main.py
@@ -1,6 +1,9 @@
+import base64
+import json
 import os
+import logging
 import requests
-from flask import Flask, request, abort
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -8,12 +11,14 @@ GITHUB_API_TOKEN = os.environ.get('GITHUB_API_TOKEN')
 GITHUB_REPO = os.environ.get('GITHUB_REPO')
 GITHUB_OWNER = os.environ.get('GITHUB_OWNER')
 
-@app.route('/', methods=['POST'])
-def create_github_issue():
-    
-    data = request.get_json()
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def create_github_issue(data):
     issue_title = f"Alert: {data['incident']['incident_id']}"
     issue_body = data['incident']['summary']
+
+    logger.info(f"Creating issue with title: {issue_title} body: {issue_body}")
 
     response = requests.post(
         f"https://api.github.com/repos/{GITHUB_OWNER}/{GITHUB_REPO}/issues",
@@ -28,9 +33,43 @@ def create_github_issue():
     )
 
     if response.status_code == 201:
+        logger.info("Issue created successfully")
         return "Issue created successfully", 201
     else:
+        logger.error(f"Failed to create issue: {response.content}")
         return f"Failed to create issue: {response.content}", response.status_code
+
+@app.route('/', methods=['POST'])
+def main(d, context): #Need to receive arguments
+    envelope = request.get_json()
+    
+    if not envelope:
+        logger.error("No envelope received")
+        return "Bad Request", 400
+    
+    logger.info(f"envelope: {envelope}")
+
+    pubsub_data = envelope.get('data', {})
+
+    if not pubsub_data:
+        logger.error(f"No outside data received: ")
+        return "Bad Request", 400
+
+    try:
+        data_base64 = pubsub_data.get('data', '')
+        if not data_base64:
+            raise ValueError("No data field in outside data")
+        
+        data = base64.b64decode(data_base64.encode('utf-8')).decode('utf-8')
+        logger.info(f"Decoded data: {data}")
+        data = json.loads(data)
+        
+        logger.info(f"Received data: {data}")
+    except Exception as e:
+        logger.error(f"Error processing message: {e}")
+        return "Bad Request", 400
+    
+    return create_github_issue(data)
 
 if __name__ == "__main__":
     app.run()
--- EasyBuggy/monitoring.tf
+++ EasyBuggy/monitoring.tf
@@ -1,9 +1,18 @@
-resource "google_monitoring_notification_channel" "webhook" {
-  display_name = "Webhook to Cloud Function"
-  type         = "webhook_tokenauth"
+resource "google_pubsub_topic" "alerts_topic" {
+  name = "alerts-topic"
+}
+
+resource "google_pubsub_subscription" "alerts_subscription" {
+  name  = "alerts-subscription"
+  topic = google_pubsub_topic.alerts_topic.name
+}
+
+resource "google_monitoring_notification_channel" "pubsub_channel" {
+  display_name = "Pub/Sub to Cloud Function"
+  type         = "pubsub"
 
   labels = {
-    "url" = "https://${var.region}-${var.project}.cloudfunctions.net/issue-creator-function"
+    "topic" = google_pubsub_topic.alerts_topic.id
   }
 }
 
@@ -19,15 +28,14 @@ resource "google_storage_bucket_object" "function_source_object" {
   source = "function-source.zip"
 }
 
-resource "google_cloudfunctions_function" "function" {
+resource "google_cloudfunctions_function" "issue_creator_function" {
   name        = "issue-creator-function"
-  description = "Receive Webhook from Google Cloud Monitoring and create a GitHub issue"
+  description = "Receive Pub/Sub message from Google Cloud Monitoring and create a GitHub issue"
 
   runtime    = "python39"
   source_archive_bucket = google_storage_bucket.easybuggy_monitoring_function_bucket.name
   source_archive_object = google_storage_bucket_object.function_source_object.name
-  trigger_http          = true
-  entry_point           = "create_github_issue"
+  entry_point           = "main"
   region                = var.region
 
   environment_variables = {
@@ -35,29 +43,28 @@ resource "google_cloudfunctions_function" "function" {
     "GITHUB_REPO"      = var.github_repo
     "GITHUB_OWNER"     = var.github_owner
   }
-}
 
-resource "google_monitoring_alert_policy" "memory_usage_policy" {
-  display_name = "High Memory Utilization Alert"
-  combiner     = "OR"
-
-  conditions {
-    display_name  = "CPU usage over 80%"
-    condition_threshold {
-      filter          = "metric.type=\"compute.googleapis.com/instance/cpu/utilization\" AND resource.type=\"gce_instance\""
-      duration        = "0s"
-      comparison      = "COMPARISON_GT"
-      threshold_value = 0.8  
-
-      aggregations {
-        alignment_period  = "60s"
-        per_series_aligner = "ALIGN_MEAN"
-      }
-    }
+  event_trigger {
+    event_type = "google.pubsub.topic.publish"
+    resource   = google_pubsub_topic.alerts_topic.id
   }
-
-
-  enabled = true
-
-  notification_channels = [google_monitoring_notification_channel.webhook.id]
 }
+
+#resource "google_monitoring_alert_policy" "cpu_usage_policy" {
+#  display_name = "High CPU Utilization Alert"
+#  combiner     = "OR"
+
+#  conditions {
+#    display_name  = "CPU usage over 80%"
+#    condition_threshold {
+#      filter          = "metric.type=\"compute.googleapis.com/instance/cpu/utilization\" AND resource.type=\"gce_instance\""
+#      duration        = "60s"
+#      comparison      = "COMPARISON_GT"
+#      threshold_value = 0.8  
+#    }
+#  }
+
+#  enabled = true
+
+#  notification_channels = [google_monitoring_notification_channel.pubsub_channel.id]
+#}
